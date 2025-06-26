dd086498cfb301ce430cddb140c5a1986e08cb62
prashanth759 <srisaiprashanthreddy@gmail.com>
2025-06-26 13:14:24 +0530

    add

--- heart_disease_prediction/settings.py
+++ heart_disease_prediction/settings.py
@@ -74,12 +74,6 @@ WSGI_APPLICATION = 'heart_disease_prediction.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -116,8 +110,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
